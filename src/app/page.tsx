daa2fb532e09371601f66dbeab95b60322184cd8
Yangray0124 <yangray0124@gmail.com>
2025-04-26 12:50:20 +0800

    add todolist and todos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,15 +3,25 @@
 import * as React from "react"
  
 import { Calendar } from "@/components/ui/calendar"
+import { TodoList } from "@/components/ui/todolist"
 
 export default function Home() {
   const [date, setDate] = React.useState<Date | undefined>(new Date())
   return (
-    <Calendar
-      mode="single"
-      selected={date}
-      onSelect={setDate}
-      className="rounded-md border shadow"
-    />
+    <>
+    <div className="flex gap-10">
+      <Calendar
+        mode="single"
+        selected={date}
+        onSelect={setDate}
+        className="rounded-md border shadow"
+      />
+      
+      <TodoList
+
+      />
+    </div>
+      
+    </>
   );
 }
