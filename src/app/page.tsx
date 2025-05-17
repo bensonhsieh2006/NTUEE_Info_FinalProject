21a0c33f665a5b9be12b79f6326633164b7212c7
Benson Hsieh <bensonhsieh2006@gmail.com>
2025-05-18 02:09:24 +0800

    beautify interface-3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -35,30 +35,34 @@ export default async function Home({
   
   return (
     <div className="flex justify-center overflow-auto">
-      <MainPage eventList={getEvent}>
-        <AllEvents pickedDate={pickedDate}></AllEvents>
-      </MainPage>
-      <div className="flex justify-center border-1 rounded-lg shadow-lg m-6 h-162">
-        <div className="row-span-2 grid grid-rows-subgrid m-4 ">
-            <div className="">
-              <TodoList>
-                  <Todos
-                      selectFinished={false}
-                  />
-              </TodoList>
-            </div>
-        </div>
-        <div className="row-span-2 grid grid-rows-subgrid m-4">
-          <div className="">
-            <CompletedTodolist>
-                <Todos
-                    selectFinished={true}
-                />
-            </CompletedTodolist>
+      <div className="w-1/2 overflow-auto">
+        <MainPage eventList={getEvent}>
+          <AllEvents pickedDate={pickedDate}></AllEvents>
+        </MainPage>
+      </div>
+      <div className="w-1/2 overflow-auto">
+        <div className="flex justify-center border-1 rounded-lg shadow-lg m-6 h-162 overflow-auto">
+          <div className="row-span-2 grid grid-rows-subgrid m-4 overflow-auto">
+              <div className="">
+                <TodoList>
+                    <Todos
+                        selectFinished={false}
+                    />
+                </TodoList>
+              </div>
           </div>
-        </div>
-        <div className="absolute bottom-4 right-4">
-            <Toaster/>
+          <div className="row-span-2 grid grid-rows-subgrid m-4 overflow-auto">
+            <div className="">
+              <CompletedTodolist>
+                  <Todos
+                      selectFinished={true}
+                  />
+              </CompletedTodolist>
+            </div>
+          </div>
+          <div className="absolute bottom-4 right-4">
+              <Toaster/>
+          </div>
         </div>
       </div>
     </div>
