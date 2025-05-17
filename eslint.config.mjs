cea400c60a13cac337471c79ee387423c56a1e70
Benson Hsieh <bensonhsieh2006@gmail.com>
2025-05-18 01:14:41 +0800

    update eslint rules-2

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,7 +12,9 @@ const compat = new FlatCompat({
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   ...compat.config({
-    rules: {"@typescript-eslint/no-unused-vars" : "off"}
+    rules: {
+      "@typescript-eslint/no-unused-vars" : "off",
+      "@typescript-eslint/no-explicit-any" : "off"}
   })
 ];
 
